528f422866c1d7f6ee3d912d6642eafc23b23c05
mahines <marcus.hines@microsoft.com>
2021-10-21 17:34:57 -0400

    tesing run

--- src/main/java/com/createcosmos/action/ActionApplication.java
+++ src/main/java/com/createcosmos/action/ActionApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ActionApplication {
-
+//comment
 	public static void main(String[] args) {
 		SpringApplication.run(ActionApplication.class, args);
 		App exampleApp = new App();
